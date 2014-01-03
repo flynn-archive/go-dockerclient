ee23e5ad404b47682c97cc93b2ffb69f5a5c007f
Jonathan Rudenberg <jonathan@titanous.com>
2014-01-03 12:04:08 -0500

    Fix tests

--- image_test.go
+++ image_test.go
@@ -19,7 +19,7 @@ import (
 
 func newTestClient(rt *FakeRoundTripper) Client {
 	endpoint := "http://localhost:4243"
-	u, _ := parseEndpoint("http://localhost:4243")
+	u, _ := parseEndpoint(endpoint)
 	client := Client{
 		endpoint:    endpoint,
 		endpointURL: u,
